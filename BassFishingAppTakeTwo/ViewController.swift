53ff8a6cdd60c488105109df68cae9725eb7f7b5
YOUR NAME <YOUR@EMAIL.com>
2016-09-21 13:36:14 -0500

    Initial Commit

---
+++ BassFishingAppTakeTwo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BassFishingAppTakeTwo
+//
+//  Created by apcsp on 9/21/16.
+//  Copyright © 2016 apcsp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
